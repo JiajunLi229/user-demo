fefc8bea3ee50e7efac408c7642e8fb0a51dfa99
JiajunLi <jiajun.li1@thoughtworks.com>
2020-11-24 10:38:45 +0800

    [JiajunLi] added module researches.

---
+++ module/hello.js
@@ -0,0 +1,6 @@
+import {user} from './user.js';
+import User from './user';
+
+console.log(user);
+
+let fucker = new User("fucker");//为什么run不起来
---
+++ module/user.js
@@ -0,0 +1,8 @@
+export let user = "john";
+//export default {User};
+
+export default class {
+    constructor(name) {
+        this.name = name;
+    }
+}
