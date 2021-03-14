5e4b8e9d856b1fde1fbecd86d516eafc3618929d
JiajunLi <jiajun.li@hometrack.com.au>
2021-03-14 15:28:29 +0800

    playing pipeline

---
+++ auto/show_environment.sh
@@ -0,0 +1,19 @@
+(
+  set -x
+
+  env | sort
+
+  uname -a
+)
+
+if [[ -x "$(command -v node)" ]]; then
+  ( set -x; node --version )
+else
+  echo "WARN: NodeJS not installed"
+fi
+
+if [[ -x "$(command -v yarn)" ]]; then
+  ( set -x; yarn --version )
+else
+  echo "WARN: Yarn not installed"
+fi
